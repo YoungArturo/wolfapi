9cd79c0c246cbc7c3337c9835d9f9b7692317cb8
mrap94 <perezarturo94@gmail.com>
2018-05-02 10:23:05 +0000

    changed userINput 7

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const waApi = WolframAlphaAPI('U72E65-ARQERXR86Y');
 //Sample problem
 //Possibly remove whitespaces
 var userInput = "prove by induction sum of j from 1 to n = n(n+1)/2 for n>0";
-//var query = urlencode(userInput);  
+var query = escape(userInput);  
     
 //mathjax-node library
 /*var mjAPI = require("mathjax-node");
